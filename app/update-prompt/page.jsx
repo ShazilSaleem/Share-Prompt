253b026d310753cf17610fb5466b1c26fa53c1eb
ShazilSaleem <shaxilsaleem@gmail.com>
2024-08-12 17:07:46 +0500

    Resolved suspense

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,9 +1,10 @@
 "use client";
-import React from "react";
+
+import React, { Suspense } from "react";
 import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
+
 const UpdatePrompt = () => {
   const router = useRouter();
   const [submitting, setSubmitting] = useState(false);
@@ -13,31 +14,33 @@ const UpdatePrompt = () => {
   });
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
-    const updatePrompt = async (e) => {
-      e.preventDefault();
-      setSubmitting(true);
-      if(!promptId) return alert("Prompt Id not found")
-      try {
-        console.log(post);
-        const response = await fetch(`/api/prompt/${promptId}`, {
-          method: "PATCH",
-          body: JSON.stringify({
-            prompt: post.prompt,
-            tag: post.tag,
-          }),
-        });
-        if (response.ok) {
-          router.push("/");
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setSubmitting(false);
+
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    if (!promptId) return alert("Prompt Id not found");
+    try {
+      console.log(post);
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
       }
-    };
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   useEffect(() => {
     const getPromptDetail = async () => {
-        console.log("PROMPT ID",promptId)
+      console.log("PROMPT ID", promptId);
       const response = await fetch(`/api/prompt/${promptId}`);
       console.log("UPDATE PROMPT", response);
       const data = await response.json();
@@ -62,4 +65,10 @@ const UpdatePrompt = () => {
   );
 };
 
-export default UpdatePrompt;
+const UpdatePromptWithSuspense = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <UpdatePrompt />
+  </Suspense>
+);
+
+export default UpdatePromptWithSuspense;
